d1ec7ed74d06bfae4e0e4a08ddcc9cf80e12684c
Salman Hasan <sh8salmanhasan@gmail.com>
2023-04-27 00:43:38 -0400

    Add existing project files to Git

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './navigation';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text className="text-red-500 text-4xl">code with nomi</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
