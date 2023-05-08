3e3a166486d6208138e7fba782a55d722e202154
Salman Hasan <sh8salmanhasan@gmail.com>
2023-05-08 19:58:14 -0400

    Add existing project files to Git

--- navigation.js
+++ navigation.js
@@ -5,6 +5,9 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
+import CartScreen from './screens/CartScreen';
+import OrderPreparingScreen from './screens/OrderPreparingScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
 
 export default function Navigation() {
   return (
@@ -16,6 +19,21 @@ export default function Navigation() {
       >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        <Stack.Screen
+          name="Cart"
+          options={{ presentation: 'modal' }}
+          component={CartScreen}
+        />
+        <Stack.Screen
+          name="OrderPreparing"
+          options={{ presentation: 'fullScreenModal' }}
+          component={OrderPreparingScreen}
+        />
+        <Stack.Screen
+          name="Delivery"
+          options={{ presentation: 'fullScreenModal' }}
+          component={DeliveryScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
